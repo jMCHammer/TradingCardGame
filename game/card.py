578569d194e6d51a6dc9fa1488e46d832e36cba9
jMCHammer <jcarey@udel.edu>
2014-04-17 16:01:36 -0400

    Adding editCollectionScreen and a few other changes

--- game/card.py
+++ game/card.py
@@ -1,19 +1,52 @@
+import spyral
+
 from question import Question
 
-class Card:
+class drawFont(spyral.Sprite):
+    def __init__(self, Scene, font, text, size):
+        spyral.Sprite.__init__(self,Scene)
+        f = spyral.Font(font, size)
+        self.image = f.render(text)
+        self.layer = "text"
+
+class Card(spyral.Sprite):
     def __init__(self, subject):
+
         self.subject = subject
         self.difficultyList = ["easy", "medium", "hard"]
         self.damage = 10
         self.hp = 100
-        self.alive = True
+        self.alive = False
+        self.clicked = False
+
+    
+    def handle_clicked(self, pos):
+        if self.collide_point(pos):
+            self.clicked = True
+
+##
+    def setPos(self, x, y):
+        self.x = x
+        self.y = y
+        self.text.pos = (self.x+10, self.y+10)
+
+##TODO Should be private
+    def draw(self, Scene):
+        spyral.Sprite.__init__(self, Scene)
+        self.image = spyral.Image(size=(100, 140)).fill((139, 69, 19))
+        self.x = 10
+        self.y = 10
+        self.text = drawFont(Scene, "Extras/Comic_Book.ttf", self.subject, 15)
+        self.text.pos = (self.x+10, self.y+10)
+        self.visible = False
+
+        spyral.event.register("input.mouse.down.left", self.handle_clicked)
 
     def isAlive(self, opponentDamage):
         self.hp = self.hp - opponentDamage
         if (self.hp <= 0):
             self.hp = 0
             self.alive = False
-        
 
     def initQuestion(self, selectedDifficulty):
         self.question = Question(self.subject, selectedDifficulty)
--- game/hero.py
+++ game/hero.py
@@ -1,29 +1,59 @@
+from card import Card
+
 class Hero:
-	"""This is our Hero class, otherwise known as the main character
-	of the game.
-	"""
-	def __init__(self, name):
-		self.name = name
-		self.deck = []
-		self.looseCards = []
+    """This is our Hero class, otherwise known as the main character
+    of the game.
+    """
+    deck       = {}
+    looseCards = {}
+    allCards   = {
+        "Addition" : Card("Addition"), 
+        "Decimal Addition" : Card("Decimal Addition"), 
+        "Algebra" : Card("Algebra"), 
+        "Geometry" : Card("Geometry")}
 
-	def addCardToDeck(self, card):
-		newcard = self.looseCards.index(card)
-		newcard.alive = True
-		self.deck.append(newcard)
-		self.looseCards.remove(card)
+    def __init__(self, name):
+        self.name = name
 
-	def addCardToLooseCards(self, card):
-		self.looseCards.append(card)
+    def addCardToDeck(self, subject):
+        if len(self.deck) < 3 and subject in self.looseCards:
+            newcard = self.looseCards[subject]
+            newcard.alive = True
+            self.deck[subject] = newcard
+            del self.looseCards[subject]
+##
+            print (self.looseCards)
+            print (self.deck)
+##
 
-	def removeCardFromDeck(self, card):
-		newcard = self.deck.index(card)
-		newcard.alive = False
-		self.looseCards.append(newcard)
-		self.deck.remove(card)
+            ## Return so referencing class can modify Card
+            return newcard
+        else:
+            print ("A deck can't have more than 3 cards OR Card isn't in looseCards")
 
-	def isAlive(self):
-		for card in deck:
-			if (card.alive):
-				return True
-		return False
+    def addCardToLooseCards(self, subject):
+        if subject in self.allCards:
+            self.looseCards[subject] = self.allCards[subject]
+
+            ## Return so referencing class can modify Card
+            return self.allCards[subject]
+        else:
+            print ("Card doesn't exist")
+
+    def removeCardFromDeck(self, subject):
+        if subject in self.deck:
+            newcard = self.deck[subject]
+            newcard.alive = False
+            self.looseCards[subject] = newcard
+            del self.deck[subject]
+            
+            ## Return so referencing class can modify Card
+            return newcard
+        else:
+            print ("subject isn't in deck")
+
+    def isAlive(self):
+        for card in self.deck:
+            if (card.alive):
+                return True
+        return False
